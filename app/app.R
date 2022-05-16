42a30d021762e66a436884efa3dff0813fbef874
bofanh <bofanh3@gmail.com>
2022-05-16 00:39:49 -0400

    add styles

--- app/app.R
+++ app/app.R
@@ -54,10 +54,26 @@ ui <- navbarPage(title = "DTSC 610 - M01/Spring 2022",
                 ),    
                 #########page Read Me##########
                 tabPanel("Interactive Map",
-                         fluidPage(
-                             leafletOutput("covidmap", width=2000, height=1000)
-                         )
+                         div(class="outer",
+                             
+                             tags$head(
+                                 # Include our custom CSS
+                                 includeCSS("styles.css"),
+                             ),
+                             
+                             leafletOutput("covidmap", width="100%", height="100%"),
+                             
+                             # Shiny versions prior to 0.11 should use class = "modal" instead.
+                             absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                           draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                           width = 330, height = "auto",
+                                           
+                                           h3("COVID-19 explorer"),
+                                           )
+                        
                 ),
+                ),
+                
                 #########page 2##########
                 tabPanel("The Datasets",
                          h2("The CDC data"),
